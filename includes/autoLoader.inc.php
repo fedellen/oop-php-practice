0702775dc7f8bad1f4b05bd36121c05471a80529
fedellen <fedellen@pixelpajamastudios.com>
2021-03-01 14:37:26 -0600

    Update autoLoader for nested 'includes' directory

--- includes/autoLoader.inc.php
+++ includes/autoLoader.inc.php
@@ -2,7 +2,15 @@
 
   /** Import classes with autoload */
   function autoLoader($class) {
-    $path = "./classes/".$class.".class.php";
-    include_once $path;
+    $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    // $path;
+    if(strpos($url, 'includes') !== false) {
+      $path = '../classes';
+    } else {
+      $path = 'classes/';
+    }
+    $extension = '.class.php';
+
+    require_once $path . $class . $extension;
   }
   spl_autoload_register('autoLoader');
